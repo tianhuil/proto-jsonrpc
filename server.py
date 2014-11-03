e1de6228d06f30a67273d6c6b65160b056865af2
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2014-11-02 20:44:30 -0500

    Tweaking port

--- server.py
+++ server.py
@@ -1,6 +1,9 @@
 from jsonrpclib.SimpleJSONRPCServer import SimpleJSONRPCServer
+import os
 
-server = SimpleJSONRPCServer(('localhost', 3030))
+port = int(os.environ.get("PORT", 5000))
+
+server = SimpleJSONRPCServer(('0.0.0.0', port))
 server.register_function(pow)
 server.register_function(lambda x,y: x+y, 'add')
 server.register_function(lambda x: x, 'ping')
